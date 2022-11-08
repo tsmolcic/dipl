b5919f29189af3931abf8aea6d8979adaafcd939
idamilankovic <ida@itgrow.hr>
2022-11-08 12:18:08 +0100

    added changes; imported two classes
    Signed-off-by: idamilankovic <ida@itgrow.hr>

--- class.py
+++ class.py
@@ -1,34 +1,9 @@
-class Employee:
-    def __init__(self, firstname, lastname, position, salary):
-        self.firstname = firstname
-        self.lastname = lastname
-        self.position = position
-        self.salary = salary
+from Employee import Employee
+from Company import Company         
 
-    def __repr__(self):
-        return  f"First name: {self.firstname}\nLast name: {self.lastname}\nPosition: {self.position}\nSalary: {self.salary}"
-
-
-
-class Company:
-    def __init__(self, name, address, employees=[]):
-        self.name = name
-        self.address = address
-        self.employees = employees
-
-    def add_employee (self, employee):
-        try:
-            if employee.salary>5000 and employee.salary<20000:
-                self.employees.append(employee)
-            else:
-                raise ValueError
-        except ValueError:
-            print('Could not add new employee; employee salary is less than 5000 or greater than 20000')
-            
-
-employee1 = Employee ('Marko', "Markić", "accountant", 6000)
+employee1 = Employee ('Marko', "Markić", "accountant", 80000)
 employee2 = Employee ('Hrvoje', "Horvat", "accountant", 8000)
-employee3 = Employee ('Ivana', "Ivić", "accountant", 25000)
+employee3 = Employee ('Ivana', "Ivić", "accountant", 2000)
 company= Company ("Firma", "zagrebačka")
 company.add_employee(employee1)
 company.add_employee(employee2)
