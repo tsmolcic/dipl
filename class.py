322bc1840047457cd198bccccd62462fb856b8c7
idamilankovic <ida@itgrow.hr>
2022-11-08 10:17:59 +0100

    made requested changes
    Signed-off-by: idamilankovic <ida@itgrow.hr>

--- class.py
+++ class.py
@@ -8,9 +8,7 @@ class Employee:
     def __repr__(self):
         return  f"First name: {self.firstname}\nLast name: {self.lastname}\nPosition: {self.position}\nSalary: {self.salary}"
 
-employee1 = Employee ('Marko', "Markić", "accountant", 6000)
-employee2 = Employee ('Hrvoje', "Horvat", "accountant", 8000)
-employee3 = Employee ('Ivana', "Ivić", "accountant", 19000)
+
 
 class Company:
     def __init__(self, name, address, employees=[]):
@@ -21,11 +19,16 @@ class Company:
     def add_employee (self, employee):
         try:
             if employee.salary>5000 and employee.salary<20000:
-                self.employees.append(employee)     
-        except:
+                self.employees.append(employee)
+            else:
+                raise ValueError
+        except ValueError:
             print('Could not add new employee; employee salary is less than 5000 or greater than 20000')
             
 
+employee1 = Employee ('Marko', "Markić", "accountant", 6000)
+employee2 = Employee ('Hrvoje', "Horvat", "accountant", 8000)
+employee3 = Employee ('Ivana', "Ivić", "accountant", 25000)
 company= Company ("Firma", "zagrebačka")
 company.add_employee(employee1)
 company.add_employee(employee2)
